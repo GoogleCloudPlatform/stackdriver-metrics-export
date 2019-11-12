a0852fe703cde7d8683ce2487b256336ea2eff8c
charlesbaer <cbaer@google.com>
2019-11-11 23:31:49 -0500

    Added last_end_time.txt logic

--- list_metrics/start.py
+++ list_metrics/start.py
@@ -15,12 +15,71 @@
 # limitations under the License.
 
 import webapp2
-
+import config
+import os
+import json
+import logging
+from datetime import datetime 
+import cloudstorage as gcs
+from cloudstorage import NotFoundError
+from google.appengine.api import app_identity
 
 class ReceiveStart(webapp2.RequestHandler):
+    def set_last_end_time(self, bucket_name):
+        """ Write the end_time as a string value in a JSON object in GCS. 
+            This file is used to remember the last end_time in case one isn't provided
+        """
+        end_time = datetime.now()
+        end_time_str = end_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+        end_time_str_json = {
+            "end_time": end_time_str
+        }
+        write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        gcs_file = gcs.open('/{}/{}'.format(
+            bucket_name, config.LAST_END_TIME_FILENAME),
+                            'w',
+                            content_type='text/plain',
+                            retry_params=write_retry_params)
+        gcs_file.write(json.dumps(end_time_str_json))
+        gcs_file.close()
+
     def get(self):
+        last_end_time_str = ""
+        try:
+            # get the App Engine default bucket name to store a GCS file with last end_time
+            bucket_name = os.environ.get('BUCKET_NAME',
+                app_identity.get_default_gcs_bucket_name()
+            )
+
+            gcs_file = gcs.open('/{}/{}'.format(
+                bucket_name, config.LAST_END_TIME_FILENAME))
+            contents = gcs_file.read()
+            logging.debug("GCS FILE CONTENTS: {}".format(contents))
+            json_contents = json.loads(contents) 
+            last_end_time_str = json_contents["end_time"]
+            gcs_file.close()
+        except NotFoundError as nfe:
+            logging.error("Missing file when reading from GCS: {}".format(nfe))
+            last_end_time_str = None
+        except Exception as e:
+            logging.error("Received error when reading from GCS: {}".format(e))
+            last_end_time_str = None
+
+        # if there is not an existing file, create one
+        try:
+            if not last_end_time_str:
+                self.set_last_end_time(bucket_name)
+        except NotFoundError as nfe:
+            logging.error("Missing file when writing to GCS: {}".format(nfe))
+            last_end_time_str = None
+        except Exception as e:
+            logging.error("Received error when writing to GCS: {}".format(e))
+            last_end_time_str = None
+    
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.status = 200
+
 
 app = webapp2.WSGIApplication([
     ('/_ah/start', ReceiveStart)
